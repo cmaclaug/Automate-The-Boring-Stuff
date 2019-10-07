d142bab015fd187d8840e2a77172e02f4b424235
cmaclaug <cmaclaug@gmail.com>
2019-10-07 08:14:26 +0100

    no message

---
+++ Section 3 - Functions/testing_spyder.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hello():
+    print ('Howdy')
+    print ('Howdy!!!')
+    print ('Hello there amigo')
+hello()
+hello()
+hello()
